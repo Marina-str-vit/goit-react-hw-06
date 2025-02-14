f34c6a66af5f69c0be88c068ec864c0194a37cce
Marina Strukova <meribsv@gmail.com>
2025-02-14 17:54:14 +0200

    rename

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
 import 'modern-normalize'
 import './index.css'
 import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './components/App';
+import './index.css';
+// 1. Імпортуємо провайдер
+import { Provider } from 'react-redux'
+// 2. Імпортуємо створений раніше стор
+import { store } from "./redux/store";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+	  <Provider store={store}>
+	    <App />
+	  </Provider>
+  </React.StrictMode>
+);
